1fdc1fc7ce55a93835687fd047f7c71bc349c549
janethesnan <afandinur111@gmail.com>
2023-01-17 05:18:54 +0700

    update suitJawa.js

--- suitJawa.js
+++ suitJawa.js
@@ -3,7 +3,7 @@ var p= prompt('pilih salah satu : gajah, semut, orang');
 //membuat pilihan komputer
 //membangkitkan bilangan random
 var comp = Math.random();
-console.log(comp);
+
 //menentukan rules
 
 //tampilkan hasil
