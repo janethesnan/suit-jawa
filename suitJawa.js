d2099fc148bdf205c6ad08f1083b24a87d0ce9ae
janethesnan <afandinur111@gmail.com>
2023-01-17 05:37:21 +0700

    menambahkan aturan game suit jawa pada suitJawa.js

--- suitJawa.js
+++ suitJawa.js
@@ -3,7 +3,23 @@ var p= prompt('pilih salah satu : gajah, semut, orang');
 //membuat pilihan komputer
 //membangkitkan bilangan random
 var comp = Math.random();
-
+if (comp <0.34){
+    comp='gajah';
+}else if(comp >= 0.34 && <=0.67){
+    comp = 'orang';
+}else{
+    comp = 'semut';
+}
 //menentukan rules
-
+var hasil= '';
+if (p==comp){
+    hasil= 'SERI';
+}else if(p='gajah'){
+    hasil=(comp=='orang') ? 'MENANG' : 'KALAH';
+}else if(p='orang'){
+    hasil=(comp=='semut') ? 'MENANG' : 'KALAH';
+}else if(p='semut'){
+    hasi=(comp=='gajah') ? 'MNANG' : 'KALAH';
+}
 //tampilkan hasil
+alert('Kamu memilih '+ p + ' dan komputer memilih '+ comp+ '. Maka hasilnya kamu '+hasil);
