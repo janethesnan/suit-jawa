241727d4d17788003bfb6739abfa44e00fd0f9e4
janethesnan <afandinur111@gmail.com>
2023-01-17 05:45:24 +0700

    bug fix

--- suitJawa.js
+++ suitJawa.js
@@ -5,7 +5,7 @@ var p= prompt('pilih salah satu : gajah, semut, orang');
 var comp = Math.random();
 if (comp <0.34){
     comp='gajah';
-}else if(comp >= 0.34 && < 0.67){
+}else if(comp >= 0.34 && comp < 0.67){
     comp = 'orang';
 }else{
     comp = 'semut';
