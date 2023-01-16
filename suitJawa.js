62474e006c42f5284e6567dc517beaaa4b4e2b6b
janethesnan <afandinur111@gmail.com>
2023-01-17 05:50:11 +0700

    menambahkan pengulangan while

--- suitJawa.js
+++ suitJawa.js
@@ -1,25 +1,32 @@
-//menangkap pilihan player
-var p= prompt('pilih salah satu : gajah, semut, orang');
-//membuat pilihan komputer
-//membangkitkan bilangan random
-var comp = Math.random();
-if (comp <0.34){
-    comp='gajah';
-}else if(comp >= 0.34 && comp < 0.67){
-    comp = 'orang';
-}else{
-    comp = 'semut';
+tanya=true;
+while(tanya){
+    //menangkap pilihan player
+    var p= prompt('pilih salah satu : gajah, semut, orang');
+    //membuat pilihan komputer
+    //membangkitkan bilangan random
+    var comp = Math.random();
+    if (comp <0.34){
+        comp='gajah';
+    }else if(comp >= 0.34 && comp < 0.67){
+        comp = 'orang';
+    }else{
+        comp = 'semut';
+    }
+    //menentukan rules
+    var hasil= '';
+    if (p==comp){
+        hasil= 'SERI';
+    }else if(p='gajah'){
+        hasil=(comp=='orang') ? 'MENANG' : 'KALAH';
+    }else if(p='orang'){
+        hasil=(comp=='semut') ? 'MENANG' : 'KALAH';
+    }else if(p='semut'){
+        hasil=(comp=='gajah') ? 'MENANG' : 'KALAH';
+    }
+    //tampilkan hasil
+    alert('Kamu memilih '+ p + ' dan komputer memilih '+ comp+ '. Maka hasilnya kamu '+hasil);
+
+    tanya=confirm('bermain lagi?');
 }
-//menentukan rules
-var hasil= '';
-if (p==comp){
-    hasil= 'SERI';
-}else if(p='gajah'){
-    hasil=(comp=='orang') ? 'MENANG' : 'KALAH';
-}else if(p='orang'){
-    hasil=(comp=='semut') ? 'MENANG' : 'KALAH';
-}else if(p='semut'){
-    hasil=(comp=='gajah') ? 'MENANG' : 'KALAH';
-}
-//tampilkan hasil
-alert('Kamu memilih '+ p + ' dan komputer memilih '+ comp+ '. Maka hasilnya kamu '+hasil);
+
+alert('Terima kasih telah bermain');
