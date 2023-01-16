f95f8c505f1d23e7577ea3b55a3f5242095a1b98
janethesnan <afandinur111@gmail.com>
2023-01-17 05:12:59 +0700

    menambahkan suitJawa.js

---
+++ suitJawa.js
@@ -0,0 +1,9 @@
+//menangkap pilihan player
+var p= prompt('pilih salah satu : gajah, semut, orang');
+//membuat pilihan komputer
+//membangkitkan bilangan random
+var comp = Math.random();
+console.log(comp);
+//menentukan rules
+
+//tampilkan hasil
